b5e57e13f686d50272a47deaed4c59fb513e6850
Zfanz12 <zulfanfalah@gmail.com>
2026-03-14 14:44:26 +0700

    melakukkan implementasi akhir and final

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmbSatuan = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNilaiAwal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNilaiAkhir = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbAkhir = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cmbSatuan
+            // 
+            this.cmbSatuan.FormattingEnabled = true;
+            this.cmbSatuan.Items.AddRange(new object[] {
+            "Celsius",
+            "Fahrenheit",
+            "Kelvin",
+            "Reamur"});
+            this.cmbSatuan.Location = new System.Drawing.Point(57, 144);
+            this.cmbSatuan.Name = "cmbSatuan";
+            this.cmbSatuan.Size = new System.Drawing.Size(69, 21);
+            this.cmbSatuan.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Satuan awal :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nilai awal :";
+            // 
+            // txtNilaiAwal
+            // 
+            this.txtNilaiAwal.Location = new System.Drawing.Point(187, 144);
+            this.txtNilaiAwal.Name = "txtNilaiAwal";
+            this.txtNilaiAwal.Size = new System.Drawing.Size(255, 20);
+            this.txtNilaiAwal.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(187, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nilai akhir :";
+            // 
+            // txtNilaiAkhir
+            // 
+            this.txtNilaiAkhir.Location = new System.Drawing.Point(187, 211);
+            this.txtNilaiAkhir.Name = "txtNilaiAkhir";
+            this.txtNilaiAkhir.ReadOnly = true;
+            this.txtNilaiAkhir.Size = new System.Drawing.Size(255, 20);
+            this.txtNilaiAkhir.TabIndex = 5;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(498, 174);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 6;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Satuan akhir :";
+            // 
+            // cmbAkhir
+            // 
+            this.cmbAkhir.FormattingEnabled = true;
+            this.cmbAkhir.Items.AddRange(new object[] {
+            "Celsius",
+            "Fahrenheit",
+            "Kelvin",
+            "Reamur"});
+            this.cmbAkhir.Location = new System.Drawing.Point(57, 226);
+            this.cmbAkhir.Name = "cmbAkhir";
+            this.cmbAkhir.Size = new System.Drawing.Size(70, 21);
+            this.cmbAkhir.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(654, 375);
+            this.Controls.Add(this.cmbAkhir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtNilaiAkhir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNilaiAwal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbSatuan);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbSatuan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNilaiAwal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNilaiAkhir;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbAkhir;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,93 @@ namespace Modul3_103022400076
         {
             InitializeComponent();
         }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            // ==================== VALIDASI COMBOBOX SATUAN AWAL ====================
+            if (cmbSatuan.SelectedIndex == -1)
+            {
+                MessageBox.Show("Pilih satuan awal terlebih dahulu!", "Peringatan",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cmbSatuan.Focus();
+                return;
+            }
+
+            // ==================== VALIDASI COMBOBOX SATUAN AKHIR ====================
+            if (cmbAkhir.SelectedIndex == -1)
+            {
+                MessageBox.Show("Pilih satuan akhir terlebih dahulu!", "Peringatan",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cmbAkhir.Focus();
+                return;
+            }
+
+            // ==================== VALIDASI TEXTBOX ====================
+            if (!double.TryParse(txtNilaiAwal.Text, out double nilai))
+            {
+                MessageBox.Show("Masukkan angka yang valid!", "Error Input",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtNilaiAwal.Focus();
+                txtNilaiAwal.SelectAll();
+                return;
+            }
+
+            // ==================== KONVERSI SUHU ====================
+            try
+            {
+                string satuanAwal = cmbSatuan.SelectedItem.ToString();
+                string satuanAkhir = cmbAkhir.SelectedItem.ToString();
+                double hasil = 0;
+
+                // STEP 1: Konversi satuan awal ke Celsius
+                double celsius = 0;
+
+                if (satuanAwal == "Celsius")
+                {
+                    celsius = nilai;
+                }
+                else if (satuanAwal == "Fahrenheit")
+                {
+                    celsius = (nilai - 32) * 5 / 9;
+                }
+                else if (satuanAwal == "Kelvin")
+                {
+                    celsius = nilai - 273.15;
+                }
+                else if (satuanAwal == "Reamur")
+                {
+                    celsius = nilai * 5 / 4;
+                }
+
+                // STEP 2: Konversi dari Celsius ke satuan akhir
+                if (satuanAkhir == "Celsius")
+                {
+                    hasil = celsius;
+                }
+                else if (satuanAkhir == "Fahrenheit")
+                {
+                    // Rumus: F = (C × 9/5) + 32
+                    hasil = (celsius * 9 / 5) + 32;
+                }
+                else if (satuanAkhir == "Kelvin")
+                {
+                    // Rumus: K = C + 273.15
+                    hasil = celsius + 273.15;
+                }
+                else if (satuanAkhir == "Reamur")
+                {
+                    // Rumus: R = C × 4/5
+                    hasil = celsius * 4 / 5;
+                }
+
+                // STEP 3: Tampilkan hasil di TextBox "Nilai Akhir"
+                txtNilaiAkhir.Text = hasil.ToString("F2");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesalahan: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
